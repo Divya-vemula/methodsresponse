0f3988a8a9a9deae225c0ffa8b3f8a87cb0f6295
Nandhini <nandhiniv122@gmail.com>
2020-02-28 10:27:29 +0530

    test_case_for_delect

---
+++ test_delect.py
@@ -0,0 +1,13 @@
+import requests
+
+# API URL
+def test_delect_employees_check_status_code_equals_204():
+ url = "https://reqres.in/api/users/2"
+
+ response = requests.delete(url)
+
+ #fetch response code
+
+ print(response.status_code)
+
+ assert response.status_code==204
