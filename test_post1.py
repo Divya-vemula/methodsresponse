2cae77fea243db9a5948e787a4bce18e195fd8e3
Nandhini <nandhiniv122@gmail.com>
2020-02-28 10:54:21 +0530

    testing_the_post

---
+++ test_post1.py
@@ -0,0 +1,14 @@
+import requests
+
+def test_post_data():
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+    payload = "{\"name\":\"test\",\"salary\":\"123\",\"age\":\"23\"}"
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+    print(response.status_code)
