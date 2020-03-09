a9f6e303b0e96f779b2e768d429f7e211a33f4ea
Nandhini <nandhiniv122@gmail.com>
2020-03-09 08:41:59 +0530

    functions

---
+++ functions/function.py
@@ -0,0 +1,13 @@
+def find_sum(n):
+
+    total=0
+
+    for i in range(n):
+
+       total=total+i
+
+    return total
+
+
+
+print (find_sum(4))
---
+++ functions/sum_of_number.py
@@ -0,0 +1,11 @@
+def find_sum(n):
+
+
+
+    for i in range(1,n+1):
+
+       print(i)
+
+
+
+print (find_sum(50))
