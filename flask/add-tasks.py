1ba015bb36c8ff6bdb3aefe9f1cdf05a33ff08ea
Nandhini <nandhiniv122@gmail.com>
2020-03-12 21:04:38 +0530

    flask

---
+++ flask/add-tasks.py
@@ -0,0 +1,56 @@
+from flask import Flask, flash, redirect, url_for, request, logging
+from flask_mysqldb import MySQL
+
+app = Flask (__name__)
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 3306
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'todo'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# TODO Update secret key
+# app.secret_key = '?'
+
+# init MYSQL
+mysql = MySQL(app)
+
+@app.route('/api/v1')
+def index():
+    return {"version": "1.0.0"}
+
+# List all tasks
+@app.route('/api/v1/tasks')
+def read_tasks():
+    cursor = mysql.connection.cursor()
+    cursor.execute('''SELECT name, description FROM todo.tasks''')
+    record_set = cursor.fetchall()
+    return str(record_set)
+
+# Add task
+@app.route('/api/v1/tasks', methods=['POST'])
+def create_tasks():
+    if request.method == 'POST':
+        name = "First Task"
+        description = "Sample first task"
+
+        # Create Cursor
+        cur = mysql.connection.cursor()
+
+        # Execute
+        cur.execute("INSERT INTO tasks(name, description) VALUES(%s, %s)", (name, description))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask/google.py
@@ -0,0 +1,8 @@
+https://accounts.google.com/o/oauth2/v2/auth?
+ scope=https://mail.google.com/&
+ access_type=offline&
+ include_granted_scopes=true&
+ state=state_parameter_passthrough_value&
+ redirect_uri=http://localhost&
+ response_type=code&
+ client_id=49271633791-rv40ba0qij03m8302k8aefviib4rgt63.apps.googleusercontent.com
